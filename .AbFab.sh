1fa8ae8419782a1292679714673a43139efcba7a
Tallicia <Tallicia@users.noreply.github.com>
2022-08-30 22:50:03 -0500

    Removing old unused sections

--- .AbFab.sh
+++ .AbFab.sh
@@ -2,31 +2,34 @@
 #shellcheck shell=bash
 #shellcheck disable=SC2016,SC1083,SC1090,SC2116,SC2154
 
-export ABFAB_SCREENSAVER_SELECT=0
-export ABFAB_SCREENSAVER_IDLE_TIME=45
-export ABFAB_SCREENSAVER_NOTICE_TIME=5
-# declare -a ABFAB_SCREEN_SAVER_LIST
-export ABFAB_SCREENSAVER_CYCLE_TIME=0
-export ABFAB_SCREEN_SAVER_LIST=("cmatrix -bu 4 -a | lolcat -F 0.00015" 
+export AbFab_SCREENSAVER_SELECT=0
+export AbFab_SCREENSAVER_IDLE_TIME=45
+export AbFab_SCREENSAVER_NOTICE_TIME=5
+# declare -a AbFab_SCREEN_SAVER_LIST
+export AbFab_SCREENSAVER_CYCLE_TIME=0
+export AbFab_SCREEN_SAVER_LIST=("cmatrix -bu 4 -a | lolcat -F 0.00015" 
 "cmatrix -bu 4 -a | lolcat -F 0.0005" 
 "cmatrix -bu 4 -a" 
 "nyancat --frames 600" 
 "asciiquarium" )
 # "hollywood -s 6" )
-export ABFAB_SCREEN_SAVER_PRESHOW_LIST=("sl -Fal | lolcat" 
+export AbFab_SCREEN_SAVER_PRESHOW_LIST=("sl -Fal | lolcat" 
 "sl -al | lolcat" 
 "nyancat --frames 100" )  # randomize the Fal 0..8 -3 bits THis is also only a preexec option it does not loop
 
-export ABFAB_EXEC_START_PREFIX='🏳️‍🌈'
-export ABFAB_EXEC_END_PREFIX='🏁 '
-export ABFAB_LOLCAT_SEED=1
-export ABFAB_LOLCAT_INCREMENT=2
-export ABFAB_LOLCAT_FREQ=0.3
-export ABFAB_ANIMAL_FRIENDS=5
-export ABFAB_ANIMAL_HERD="false"
-# ABFAB_ANIMAL_SET_FRIENDS 3
-export ABFAB_ANIMAL_SELECT=0  # Set to 0 for random
-export ABFAB_ANIMALS=🐒🦍🐕🐩🐈🐅🐆🐴🐎🦄🦓🦌🐂🐃🐄🐖🐗🐏🐑🐐🐪🐫🦙🦒🐘🦏🦛🐁🐀🐇🐿🦔🦇🦘🦡🦃🐔🐓🐣🐤🐥🐦🐧🕊🦅🦆🦢🦉🦚🦜🐊🐢🦎🐍🐲🐉🦕🦖🐳🐋🐬🐟🐠🐡🦈🐙🐚🦀🦞🦐🦑🐌🦋🐛🐜🐝🐞🦗🕷🦂
+export AbFab_EXEC_START_PREFIX='🏳️‍🌈'
+export AbFab_EXEC_END_PREFIX='🏁 '
+export AbFab_LOLCAT_SEED=1
+export AbFab_LOLCAT_INCREMENT=2
+export AbFab_LOLCAT_FREQ=0.3
+export AbFab_PROMPT_COLOR='rainbow'
+export AbFab_ANIMAL_FRIENDS=5
+export AbFab_ANIMAL_FRIENDS_MIN=2
+export AbFab_ANIMAL_FRIENDS_MAX=14
+export AbFab_ANIMAL_HERD="false"
+# AbFab_ANIMAL_SET_FRIENDS 3
+export AbFab_ANIMAL_SELECT=0  # Set to 0 for random
+export AbFab_ANIMALS=🐒🦍🐕🐩🐈🐅🐆🐴🐎🦄🦓🦌🐂🐃🐄🐖🐗🐏🐑🐐🐪🐫🦙🦒🐘🦏🦛🐁🐀🐇🐿🦔🦇🦘🦡🦃🐔🐓🐣🐤🐥🐦🐧🕊🦅🦆🦢🦉🦚🦜🐊🐢🦎🐍🐲🐉🦕🦖🐳🐋🐬🐟🐠🐡🦈🐙🐚🦀🦞🦐🦑🐌🦋🐛🐜🐝🐞🦗🕷🦂
 
 #🌈🏳️‍🌈
 
@@ -52,22 +55,22 @@ export ABFAB_ANIMALS=🐒🦍🐕🐩🐈🐅🐆🐴🐎🦄🦓🦌🐂🐃�
 
 #🌿🌽🌶🍞🍟🍕🍲🍚🍜☕🔪
 
-#export ABFAB_SCREEN_SAVER_DISABLE=true # This turns off the terminal screensaver
-#unset ABFAB_SCREEN_SAVER_DISABLE  # This resumes screen saver timeout 
+#export AbFab_SCREEN_SAVER_DISABLE=true # This turns off the terminal screensaver
+#unset AbFab_SCREEN_SAVER_DISABLE  # This resumes screen saver timeout 
 
-#export ABFAB_RUN_DURATION_DISABLE=true # This turns off the duration feature
-#unset ABFAB_RUN_DURATION_DISABLE  # This resumes duration feature
+#export AbFab_RUN_DURATION_DISABLE=true # This turns off the duration feature
+#unset AbFab_RUN_DURATION_DISABLE  # This resumes duration feature
 
-ABFAB_ANIMAL_ARRAY=($(eval echo "$ABFAB_ANIMALS" | sed "s/./& /g"))
-# mapfile -t ABFAB_ANIMAL_ARRAY < <($ABFAB_ANIMALS)
-# animal_emoji=${ABFAB_ANIMAL_ARRAY[$ABFAB_ANIMAL_SELECT]}
+AbFab_ANIMAL_ARRAY=($(eval echo "$AbFab_ANIMALS" | sed "s/./& /g"))
+# mapfile -t AbFab_ANIMAL_ARRAY < <($AbFab_ANIMALS)
+# animal_emoji=${AbFab_ANIMAL_ARRAY[$AbFab_ANIMAL_SELECT]}
 
 setopt promptsubst
 
-alias ABFAB_SETTINGS='export | rg ABFAB'
+alias AbFab_SETTINGS='export | rg AbFab'
 
-ABFAB_DEPENDENCY_LIST() {
-  export ABFAB_DEPENDENCIES=("coreutils" 
+AbFab_DEPENDENCY_LIST() {
+  export AbFab_DEPENDENCIES=("coreutils" 
   "tmux" 
   "cmatrix" 
   "cowsay" 
@@ -78,96 +81,112 @@ ABFAB_DEPENDENCY_LIST() {
   "nyancat" "sl" )
 }
 
-ABFAB_ANIMAL() {
+AbFab_ANIMAL() {
   if [[ $1 -gt 0 ]]; then
-    export ABFAB_ANIMAL_SELECT=$1
+    export AbFab_ANIMAL_SELECT=$1
   else
-   export ABFAB_ANIMAL_SELECT=0
+   export AbFab_ANIMAL_SELECT=0
   fi
-  echo "ABFAB_ANIMAL_SELECT: $ABFAB_ANIMAL_SELECT  (Reminder: 0 is random)"
+  echo "AbFab_ANIMAL_SELECT: $AbFab_ANIMAL_SELECT  (Reminder: 0 is random)"
 }
 
-ABFAB_ANIMAL_SET_FRIENDS() {
-  if [[ $1 -gt 0 ]]; then
-    export ABFAB_ANIMAL_FRIENDS=$1
+AbFab_ANIMAL_FRIENDS_SET_NUM() {
+  if [[ $1 -gt $AbFab_ANIMAL_FRIENDS_MIN && $1 -le $AbFab_ANIMAL_FRIENDS_MAX ]]; then
+    AbFab_ANIMAL_FRIENDS=$1
+  elif [[ $1 -le $AbFab_ANIMAL_FRIENDS_MIN ]]; then
+    AbFab_ANIMAL_FRIENDS=$AbFab_ANIMAL_FRIENDS_MIN
+  else
+    AbFab_ANIMAL_FRIENDS=$AbFab_ANIMAL_FRIENDS_MAX
   fi
-  echo "ABFAB_ANIMAL_FRIENDS: $ABFAB_ANIMAL_FRIENDS "
+  if [[ $1 -eq -1 ]]; then
+    AbFab_ANIMAL_FRIENDS=-1
+  fi
+  export AbFab_ANIMAL_FRIENDS
+  echo "AbFab_ANIMAL_FRIENDS: $AbFab_ANIMAL_FRIENDS "
 }
 
-ABFAB_SHOW_ANIMAL_FRIENDS_HERD() {
+AbFab_SHOW_ANIMAL_FRIENDS_HERD() {
   animal_friends=""
   i=0
-  if [[ "$ABFAB_ANIMAL_HERD" = "true" ]]; then
-    herd_index=$((RANDOM % ${#ABFAB_ANIMAL_ARRAY[@]}))
+  if [[ "$AbFab_ANIMAL_HERD" = "true" ]]; then
+    herd_index=$((RANDOM % ${#AbFab_ANIMAL_ARRAY[@]}))
   fi
-  until [[ $i -ge $ABFAB_ANIMAL_FRIENDS ]]; do
-    if [[ "$ABFAB_ANIMAL_SELECT" -eq 0 ]]; then
-      if [[ "$ABFAB_ANIMAL_HERD" = "true" ]]; then
+  until [[ $i -ge $AbFab_ANIMAL_FRIENDS ]]; do
+    if [[ "$AbFab_ANIMAL_SELECT" -eq 0 ]]; then
+      if [[ "$AbFab_ANIMAL_HERD" = "true" ]]; then
         idx=$herd_index
       else
-        idx=$((RANDOM % ${#ABFAB_ANIMAL_ARRAY[@]}))
+        idx=$((RANDOM % ${#AbFab_ANIMAL_ARRAY[@]}))
       fi
-      animal_friends+=${ABFAB_ANIMAL_ARRAY[$idx+1]}
+      animal_friends+=${AbFab_ANIMAL_ARRAY[$idx+1]}
     else
-      animal_friends+=${ABFAB_ANIMAL_ARRAY[$ABFAB_ANIMAL_SELECT]}
+      animal_friends+=${AbFab_ANIMAL_ARRAY[$AbFab_ANIMAL_SELECT]}
     fi
     ((i++))
   done
   echo "$animal_friends"
 }
 
-ABFAB_SHOW_ANIMAL_FRIENDS() {
+AbFab_SHOW_ANIMAL_FRIENDS() {
   animal_friends=""
   i=0
-  until [[ $i -ge $ABFAB_ANIMAL_FRIENDS ]]; do
-    if [[ "$ABFAB_ANIMAL_SELECT" = "0" ]]; then
-      animal_index=$((RANDOM % ${#ABFAB_ANIMAL_ARRAY[@]}))
-      animal_friends+=${ABFAB_ANIMAL_ARRAY[$animal_index+1]}
+  until [[ $i -ge $AbFab_ANIMAL_FRIENDS ]]; do
+    if [[ "$AbFab_ANIMAL_SELECT" = "0" ]]; then
+      animal_index=$((RANDOM % ${#AbFab_ANIMAL_ARRAY[@]}))
+      animal_friends+=${AbFab_ANIMAL_ARRAY[$animal_index+1]}
     else
-      animal_friends+=${ABFAB_ANIMAL_ARRAY[$ABFAB_ANIMAL_SELECT]}
+      animal_friends+=${AbFab_ANIMAL_ARRAY[$AbFab_ANIMAL_SELECT]}
     fi
     ((i++))
   done
   export animal_friends
 }
 
-ABFAB_PS1_ANIMALS() {
-  if [[ "$ABFAB_ANIMAL_HERD" = "true" ]]; then
-    ABFAB_SHOW_ANIMAL_FRIENDS_HERD "$@"
+AbFab_PS1_ANIMALS() {
+  if [[ "$AbFab_ANIMAL_HERD" = "true" ]]; then
+    AbFab_SHOW_ANIMAL_FRIENDS_HERD "$@"
   else
-    ABFAB_SHOW_ANIMAL_FRIENDS
+    AbFab_SHOW_ANIMAL_FRIENDS
     echo "$animal_friends"
   fi
 }
 
-ABFAB_RAINBOW() {
-  lolcat -f -F $ABFAB_LOLCAT_FREQ -S $ABFAB_LOLCAT_SEED
+AbFab_RAINBOW() {
+  lolcat -f -F $AbFab_LOLCAT_FREQ -S $AbFab_LOLCAT_SEED
 }
 
-ABFAB_RAINBOW_INC() {
-  ABFAB_RAINBOW
-  ((ABFAB_LOLCAT_SEED += ABFAB_LOLCAT_INCREMENT))
+AbFab_COLOR() {  #TODO:This needs to be completed, single color prompt not working
+  if [[ "$1" != "" ]]; then
+    echo '%{$fg['$1']%}'
+  else
+    echo '%{$fg[$AbFab_PROMPT_COLOR]%}'
+  fi
 }
 
-ABFAB_SHOW_ALL_ANIMAL_NUMBERS() {
+AbFab_RAINBOW_INC() {
+  AbFab_RAINBOW
+  ((AbFab_LOLCAT_SEED += AbFab_LOLCAT_INCREMENT))
+}
+
+AbFab_SHOW_ALL_ANIMAL_NUMBERS() {
   out='Animals with Numbers:\n'
-  for (( x = 1; x <= $#ABFAB_ANIMAL_ARRAY; x++ ))
+  for (( x = 1; x <= $#AbFab_ANIMAL_ARRAY; x++ ))
   do
-    out+="$x ${ABFAB_ANIMAL_ARRAY[$x]} "
+    out+="$x ${AbFab_ANIMAL_ARRAY[$x]} "
   done
-  echo "$out" | ABFAB_RAINBOW
+  echo "$out" | AbFab_RAINBOW
 }
 
-ABFAB_SHOW_ALL_ANIMALS() {
-  echo $ABFAB_ANIMALS
+AbFab_SHOW_ALL_ANIMALS() {
+  echo $AbFab_ANIMALS
 }
 
-ABFAB_SHOW_ANIMAL() {
+AbFab_SHOW_ANIMAL() {
   if [[ $1 -gt 0 ]]; then
-    echo "$1 is ${ABFAB_ANIMAL_ARRAY[$1]}" | ABFAB_RAINBOW
+    echo "$1 is ${AbFab_ANIMAL_ARRAY[$1]}" | AbFab_RAINBOW
   else
     echo "Please enter a number to see that animal, but here they all are."
-    ABFAB_SHOW_ALL_ANIMAL_NUMBERS "$@"
+    AbFab_SHOW_ALL_ANIMAL_NUMBERS "$@"
   fi
 }
 
@@ -177,39 +196,39 @@ __prev_cmd() {
   return "$cmd"
 }
 
-ABFAB_DURATION() {
-  if [[ -v start_cmd && ! -v ABFAB_RUN_DURATION_DISABLE ]]; then
-    end_cmd=$(ABFAB_EXEC_TS)
+AbFab_DURATION() {
+  if [[ -v start_cmd && ! -v AbFab_RUN_DURATION_DISABLE ]]; then
+    end_cmd=$(AbFab_EXEC_TS)
     cmd_dur="$(bc <<<"$end_cmd-$start_cmd")"
-    # echo ${TERM_BLUE}"Duration: ${cmd_dur}"${TERM_DEFAULT} | ABFAB_RAINBOW_INC
+    # echo ${TERM_BLUE}"Duration: ${cmd_dur}"${TERM_DEFAULT} | AbFab_RAINBOW_INC
     prev_hist=$HISTCMD && ((prev_hist-=1))
     prev=$(echo "${history[$prev_hist]}")
-    fin=$(echo "$ABFAB_EXEC_END_PREFIX" "$(get_date_blink_colon)" Duration "$cmd_dur" - "$prev" | ABFAB_RAINBOW_INC )
+    fin=$(echo "$AbFab_EXEC_END_PREFIX" "$(get_date_blink_colon)" Duration "$cmd_dur" - "$prev" | AbFab_RAINBOW_INC )
     echo "$fin"
     done=""
     if [ "$PS1_exit" -ne 0 ]; then
       done+="🚩"
     fi
     done+="🏁 $prev_hist $prev Completed Duration: $cmd_dur"
-    ABFAB_TERM_TITLE "$done"
+    AbFab_TERM_TITLE "$done"
     unset start_cmd; unset end_cmd; unset cmd_dur
   fi
 }
 
-ABFAB_EXEC_TS() {
+AbFab_EXEC_TS() {
   date -u +%s.%N
 }
 
-ABFAB_START_EXEC() {
-  if [[ ! -v ABFAB_RUN_DURATION_DISABLE ]]; then
-    start_cmd=$(ABFAB_EXEC_TS)
-    #echo "$ABFAB_EXEC_START_PREFIX $(get_date_blink_colon)-Run Start from: $(pwd)" | ABFAB_RAINBOW_INC
+AbFab_START_EXEC() {
+  if [[ ! -v AbFab_RUN_DURATION_DISABLE ]]; then
+    start_cmd=$(AbFab_EXEC_TS)
+    #echo "$AbFab_EXEC_START_PREFIX $(get_date_blink_colon)-Run Start from: $(pwd)" | AbFab_RAINBOW_INC
   fi
 }
 
-ABFAB_TERM_TITLE() { printf "\033]0;%s\007" "$*"; }
+AbFab_TERM_TITLE() { printf "\033]0;%s\007" "$*"; }
 
-ABFAB_START_TITLE() {
+AbFab_START_TITLE() {
   cmd=""
   if [[ "$1" != "" ]]; then
    cmd=$1
@@ -217,44 +236,44 @@ ABFAB_START_TITLE() {
     cmd_hist=$HISTCMD && ((prev_hist-=1))
     cmd=$(echo "${history[$cmd_hist]}")
   fi
-  start_run=$(echo "🏳️‍🌈 $(get_date_blink_colon) Started: $cmd" | ABFAB_RAINBOW_INC )
+  start_run=$(echo "🏳️‍🌈 $(get_date_blink_colon) Started: $cmd" | AbFab_RAINBOW_INC )
   echo "$start_run"
-  ABFAB_TERM_TITLE "🏳️‍🌈 $cmd_hist Started Running : $cmd"
+  AbFab_TERM_TITLE "🏳️‍🌈 $cmd_hist Started Running : $cmd"
 }
 
 preexec() {
-  ABFAB_START_TITLE
-  ABFAB_START_EXEC
+  AbFab_START_TITLE
+  AbFab_START_EXEC
 }
 
-ABFAB_COUNTDOWN () {
+AbFab_COUNTDOWN () {
   i="$1"
   while [ "$i" -ge 0 ]; do
-    printf "\rStarting in %s seconds." "$i" | ABFAB_RAINBOW
+    printf "\rStarting in %s seconds." "$i" | AbFab_RAINBOW
     ((i -= 1))
     sleep 1
   done
-  printf '\nStarting now.\n' | ABFAB_RAINBOW_INC
+  printf '\nStarting now.\n' | AbFab_RAINBOW_INC
 }
 
-ABFAB_SCREENSAVER() {
+AbFab_SCREENSAVER() {
   screen_saver_on="true"
   ind=$1
   if [[ $1 -eq 0 ]]; then
-    printf "\nRandomly selecting screen saver. Iteractively, provide a number to choose specific screensaver.\n" | ABFAB_RAINBOW_INC
-    ind=$((RANDOM % ${#ABFAB_SCREEN_SAVER_LIST[@]} + 1))
+    printf "\nRandomly selecting screen saver. Iteractively, provide a number to choose specific screensaver.\n" | AbFab_RAINBOW_INC
+    ind=$((RANDOM % ${#AbFab_SCREEN_SAVER_LIST[@]} + 1))
   fi
-  chosen=${ABFAB_SCREEN_SAVER_LIST[ind]}
-  echo "$(get_date) Starting the chosen one: ${chosen}" | ABFAB_RAINBOW_INC
-  #sleep $ABFAB_SCREENSAVER_NOTICE_TIME
-  ABFAB_COUNTDOWN $ABFAB_SCREENSAVER_NOTICE_TIME
-  #timeout --preserve-status --kill-after=$ABFAB_SCREENSAVER_NOTICE_TIME > nul
-  ABFAB_START_TITLE "$chosen"
-  ABFAB_START_EXEC 
+  chosen=${AbFab_SCREEN_SAVER_LIST[ind]}
+  echo "$(get_date) Starting the chosen one: ${chosen}" | AbFab_RAINBOW_INC
+  #sleep $AbFab_SCREENSAVER_NOTICE_TIME
+  AbFab_COUNTDOWN $AbFab_SCREENSAVER_NOTICE_TIME
+  #timeout --preserve-status --kill-after=$AbFab_SCREENSAVER_NOTICE_TIME > nul
+  AbFab_START_TITLE "$chosen"
+  AbFab_START_EXEC 
   eval "${chosen}"
-  # echo "$(get_date) '-' Screensaver exited." | ABFAB_RAINBOW_INC
-  ABFAB_DURATION
-  START=$SECONDS+$ABFAB_SCREENSAVER_IDLE_TIME
+  # echo "$(get_date) '-' Screensaver exited." | AbFab_RAINBOW_INC
+  AbFab_DURATION
+  START=$SECONDS+$AbFab_SCREENSAVER_IDLE_TIME
 }
 
 if [ -f ~/scripts/.git_prompt.sh ]; then
@@ -316,43 +335,43 @@ function get_random() {
 }
 
 function get_abfab_animals() {
-  ABFAB_SHOW_ANIMAL_FRIENDS_RETURN "$(get_random)"
+  AbFab_SHOW_ANIMAL_FRIENDS_RETURN "$(get_random)"
 }
 
 precmd_functions+=('__prompt_command')
 # precmd_functions=('__prompt_command' "${precmd_functions[@]}")  # this is to prepend
-PS1_COLORIZE_COMMAND='__ps1_abfab'
+AbFab_COLORIZE='__abfab'
 
-export PS1_DEBUG_COMMAND='__ps1_debug'
-export PS1_DEBUG='false'
+# export PS1_DEBUG_COMMAND='__ps1_debug'
+# export PS1_DEBUG='false'
 
 __prompt_command() {
   PS1_exit=$? # Save last exit code
 
-  ABFAB_DURATION
-  ABFAB_SHOW_ANIMAL_FRIENDS
+  AbFab_DURATION
+  AbFab_SHOW_ANIMAL_FRIENDS
 
-  if [[ "$PS1_DEBUG" = 'true' ]]; then
-    if [[ "$PS1" = "$PS1_DEBUG_DISABLED" ]]; then
-      PS1=$PS1_DEBUG_ENABLED
-    fi
-  else
-    if [[ "$PS1" = "$PS1_DEBUG_ENABLED" ]]; then
-      PS1=$PS1_DEBUG_DISABLED
-    fi
-  fi
+  # if [[ "$PS1_DEBUG" = 'true' ]]; then
+  #   if [[ "$PS1" = "$PS1_DEBUG_DISABLED" ]]; then
+  #     PS1=$PS1_DEBUG_ENABLED
+  #   fi
+  # else
+  #   if [[ "$PS1" = "$PS1_DEBUG_ENABLED" ]]; then
+  #     PS1=$PS1_DEBUG_DISABLED
+  #   fi
+  # fi
 
-  ((ABFAB_LOLCAT_SEED += ABFAB_LOLCAT_INCREMENT))
+  ((AbFab_LOLCAT_SEED += AbFab_LOLCAT_INCREMENT))
   START=$SECONDS
 }
 
-ps1_set_debug_mode() {
-  if [[ "$PS1_DEBUG" = 'true' ]]; then
-    PS1=$PS1_DEBUG_ENABLED
-  else
-    PS1=$PS1_DEBUG_DISABLED
-  fi
-}
+# ps1_set_debug_mode() {
+#   if [[ "$PS1_DEBUG" = 'true' ]]; then
+#     PS1=$PS1_DEBUG_ENABLED
+#   else
+#     PS1=$PS1_DEBUG_DISABLED
+#   fi
+# }
 
 TERM_DEFAULT=$(tput sgr0)
 TERM_RED=$(tput setaf 1)
@@ -370,22 +389,22 @@ PS1_GREEN_LITERAL=$SOH$TERM_GREEN$STX
 PS1=$(get_prev_result)
 
 #Seperate Rainbow per section ( date, path, git, etc )
-# PS1+='$(eval "$PS1_COLORIZE_COMMAND" <<<$(get_date))'
-# PS1+=' $(eval "$PS1_COLORIZE_COMMAND" <<<"(\w)")'
-# PS1+=' $(eval "$PS1_COLORIZE_COMMAND" <<<$(parse_git_set_info))'
+# PS1+='$(eval "$AbFab_COLORIZE" <<<$(get_date))'
+# PS1+=' $(eval "$AbFab_COLORIZE" <<<"(\w)")'
+# PS1+=' $(eval "$AbFab_COLORIZE" <<<$(parse_git_set_info))'
 
 #Single Rainbow with blinking colons
-PS1+='$(eval "$PS1_COLORIZE_COMMAND" <<< "$(ABFAB_PS1_ANIMALS)$(get_date_blink_colon)$(ABFAB_PS1_ANIMALS)$(parse_git_set_info)($(print -P "%~"))!>")'
+PS1+='$(eval "$AbFab_COLORIZE" <<< "$(AbFab_PS1_ANIMALS | rev)$(get_date_blink_colon)$(AbFab_PS1_ANIMALS)$(parse_git_set_info)($(print -P "%~"))!>")'
 
-PS1_DEBUG_ENABLED='$(eval "$PS1_DEBUG_COMMAND" <<<"'$PS1'")'
-PS1_DEBUG_ENABLED='$(__ps1_escape_percent <<<"'$PS1_DEBUG_ENABLED'")'
+# PS1_DEBUG_ENABLED='$(eval "$PS1_DEBUG_COMMAND" <<<"'$PS1'")'
+# PS1_DEBUG_ENABLED='$(__ps1_escape_percent <<<"'$PS1_DEBUG_ENABLED'")'
 
-PS1_DEBUG_DISABLED=$PS1
+# PS1_DEBUG_DISABLED=$PS1
 
-__ps1_remove_newline() {
+__remove_newline() {
   tr -d '\n'
 }
-__ps1_color_wrap_non_printing() {
+__color_wrap_non_printing() {
   local REGEX=$ESC'\[[[:digit:];]*m'
   local REPLACE=$SOH'&'$STX
 #   local REPLACE=%{$SOH&$STX%}
@@ -396,8 +415,12 @@ __collapse_non_printing() {
   sed -E "s/$SOH/%{/g" -E "s/$STX/%}/g"
 }
 
-__ps1_abfab() {
-  ABFAB_RAINBOW | __ps1_color_wrap_non_printing 
+__abfab() {
+  if [[ "$AbFab_PROMPT_COLOR" = "rainbow" ]]; then
+    AbFab_RAINBOW | __color_wrap_non_printing 
+  else 
+    AbFab_COLOR $AbFab_PROMPT_COLOR | __color_wrap_non_printing
+  fi
 }
 
 __ps1_colorize_debug() {
@@ -413,23 +436,24 @@ __ps1_colorize_debug() {
 
   sed -E "s/$REGEX/$REPLACE/g"
 }
-__ps1_debug() {
-  cat -v | __ps1_colorize_debug
-}
 
-__ps1_debug_sed() {
-  # sed -e 's/'$ESC'/[ESC]/g' -e 's/'$SOH'/[SOH]/g' -e 's/'$STX'/[STX]/g'
-  sed -e "s/$ESC/[ESC]/g" -e "s/$SOH/[SOH]/g" -e "s/$STX/[STX]/g"
-}
+# __ps1_debug() {
+#   cat -v | __ps1_colorize_debug
+# }
+
+# __ps1_debug_sed() {
+#   # sed -e 's/'$ESC'/[ESC]/g' -e 's/'$SOH'/[SOH]/g' -e 's/'$STX'/[STX]/g'
+#   sed -e "s/$ESC/[ESC]/g" -e "s/$SOH/[SOH]/g" -e "s/$STX/[STX]/g"
+# }
 
 __ps1_escape_percent() {
   sed 's/%/%&/g'
 }
 
 TRAPALRM () {
-  if [ $((SECONDS-START)) -ge $ABFAB_SCREENSAVER_IDLE_TIME ]; then
-    if [[ ! -v ABFAB_SCREEN_SAVER_DISABLE && ! -v screen_saver_on ]]; then
-      ABFAB_SCREENSAVER $ABFAB_SCREENSAVER_SELECT  # randomly selects by default
+  if [ $((SECONDS-START)) -ge $AbFab_SCREENSAVER_IDLE_TIME ]; then
+    if [[ ! -v AbFab_SCREEN_SAVER_DISABLE && ! -v screen_saver_on ]]; then
+      AbFab_SCREENSAVER $AbFab_SCREENSAVER_SELECT  # randomly selects by default
     fi
   else
     unset screen_saver_on
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-AbFab placeholder for AbFab shell theme
+# AbFab shell theme
 
-Includes:
+## Includes:
+<ul>
 <li>Realishtime (1s) prompt ppdate of git status, time, etc
 <li>ScreenSaver automatically runs after idle time
 <li>Animal Farm herding and randomization
 <li>
+<li> Smooth Gradient rainbow that maintains consistency
+<li> Process Execution Duration
+<li> Title Bar Integration
+</ul>
